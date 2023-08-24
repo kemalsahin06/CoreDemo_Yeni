c1b94db7a2f6a8410b284d194643220e2b4a156b
kemal sahin <kemalshnn267@gmail.com>
2023-08-24 08:10:18 +0300

    Add project files.

---
+++ CoreDemoYeni/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDemoYeni.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
